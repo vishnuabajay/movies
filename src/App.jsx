f858aa251ae39758fcf46ceec3fd57c110f461a4
vishnuabajay <vishnuabajayab123@gmail.com>
2026-06-17 20:50:30 +0530

    Configure routing and redirection for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { MovieDetails } from './pages/MovieDetails/MovieDetails'
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
